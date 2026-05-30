6a6010b3db05ae508ce00f853b51605dabeb34d1
Michela <m.gaeta29@studenti.unisa.it>
2026-05-30 12:47:42 +0200

    Risoluzione conflitti

--- src/main/java/com/Main.java
+++ src/main/java/com/Main.java
@@ -1,10 +1,6 @@
 package com;
 
-<<<<<<< HEAD
-=======
 import com.Model.Library;
-
->>>>>>> db60258 (Metodo di rimozione traccia)
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
@@ -16,10 +12,6 @@ public class Main extends Application {
     public void start(Stage pStage) throws Exception {
         FXMLLoader loader = new FXMLLoader(getClass().getResource("/com/View/MainView.fxml"));
         Parent p = loader.load();
-<<<<<<< HEAD
-=======
-        
->>>>>>> db60258 (Metodo di rimozione traccia)
         pStage.setTitle("Riproduttore musicale");
         pStage.setScene(new Scene(p, 1000, 650));
         pStage.show();
